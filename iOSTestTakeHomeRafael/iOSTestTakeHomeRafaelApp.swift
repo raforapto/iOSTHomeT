83f3f7ab09910259ca26c9f8e2bfc5c18896b370
Rafael Mirza <rafael.psionet@me.com>
2022-10-05 17:14:23 +0100

    Initial Commit

---
+++ iOSTestTakeHomeRafael/iOSTestTakeHomeRafaelApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSTestTakeHomeRafaelApp.swift
+//  iOSTestTakeHomeRafael
+//
+//  Created by Rafael Mirza on 05/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct iOSTestTakeHomeRafaelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
