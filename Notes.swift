71e93763ddacaf62d239dc7f0a9e7cfcf88786b1
Rafael Mirza <rafael.psionet@me.com>
2022-10-09 22:46:27 +0100

    fin

---
+++ Notes.swift
@@ -0,0 +1,9 @@
+// This is fairly rushed, as I did multiple of these take hoome tests this week, so have limited myself to the suggested 3 hours.
+
+// Regarding the question of how to implement an offline mode, I would figure out a way of mapping the models used into core Data entities, and then rewriting a set of entities every time data is pulled from the two api's, therefore leading to the most recent version always being available - i'm not sure this would take very long.
+
+// Regarding the use of combine - I haven't had to do any networking code in my current role as a Junior iOS Engineer at RSLTN, however
+// I did manage to figure out enough of the SDK from the apple documentation to get it up and running, though in the very limited time
+// I've spent, i'm sure it is far from perfect.
+
+// I would also say that my testing in this take home test has room for improvement - I should use mocks and dependency injection to simulate my networking class, however I haven't had to do this before so wouldn't have been able to learn how to do this within the 3 hour time limit.
